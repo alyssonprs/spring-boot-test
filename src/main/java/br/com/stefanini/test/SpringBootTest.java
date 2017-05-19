4e0cd6b30624d6bfec648f88264a2ed20e7a10b1
Vitor Oliveira <vtr.oliveira@icloud.com>
2017-05-19 14:59:28 -0300

    first commit

---
+++ src/main/java/br/com/stefanini/test/SpringBootTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.stefanini.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author stefanini
+ */
+@SpringBootApplication
+public class SpringBootTest {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTest.class, args);
+    }
+}
